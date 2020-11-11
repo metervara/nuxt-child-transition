6d1ac9ce40d9dcb04e8d5dc225096665b42d7631
Patrik Svensson <ps@molamil.com>
2020-11-11 11:28:40 +0100

    first commit

---
+++ pages/child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      CHILD ROOT
+    </h1>
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  key(route) {
+    return route.fullPath
+  },
+}
+</script>
---
+++ pages/child/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="child">
+    <h1>CHILD: {{$route.path}}</h1>
+    <nuxt-content :document="page" />
+  </div>
+</template>
+
+<script>
+export default {
+  transition: {
+    name: 'custom',
+  },
+  key(route) {
+    return route.fullPath
+  },
+  async asyncData ({ params, $content }) {
+    // console.log(params.id)
+    const page = await $content(params.id).fetch()
+
+    return {
+      page
+    }
+  }
+}
+</script>
